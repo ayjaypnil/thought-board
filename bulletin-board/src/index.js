59272a38864f04307373a5de213e7343a696fdbe
Anil Patel <ajpatel92@icloud.com>
2018-04-25 16:58:59 -0500

    bboard push

---
+++ bulletin-board/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Board from './Board'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render (
+    <Board count={50}/>, 
+    document.getElementById('root')
+)
+registerServiceWorker()
